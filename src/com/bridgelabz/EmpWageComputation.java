7eb5dd2e5de93fd66cade82ac6ac94eec6fc5ac1
renuka13599 <renuka13599@gmail.com>
2021-10-23 11:52:31 +0530

    Added:Employee monthly wage

--- src/com/bridgelabz/EmpWageComputation.java
+++ src/com/bridgelabz/EmpWageComputation.java
@@ -1,29 +1,38 @@
 package com.bridgelabz;
 
 public class EmpWageComputation {
-    public static final double IS_FULL_TIME = 8;
-    public static final double IS_PART_TIME = 4;
-    public static final double EMP_WAGE_PER_HR = 20;
+        public static final int part_time = 1;
+        public static final int full_time = 2;
+        public static final int wage_per_hour = 20;
+        public static final int number_of_working_days=20;
 
-    public static void main(String[] args) {
-        double Empcheck = (double) Math.floor(Math.random() * 10) % 3;
-        double calcDailyWage = 0;
-        if (Empcheck == 2) {
-            System.out.println("Employee is present");
-            calcDailyWage = IS_FULL_TIME * EMP_WAGE_PER_HR;
-            System.out.printf("Calculate daily wage:" +calcDailyWage);
 
-        } else if (Empcheck == 1) {
-            calcDailyWage = IS_PART_TIME * EMP_WAGE_PER_HR;
-            System.out.println("Part time wage is:");
-            System.out.println("Calculate daily wage:" + calcDailyWage);
-        } else {
-            System.out.println("Employee is absent");
+        public static void main(String[] args) {
+            int working_hours = 0;
+            int  salary = 0;
+            int totalsalary = 0;
+            for (int day = 0; day < number_of_working_days ; day++) {
+                int empcheck = (int) Math.floor(Math.random() * 10) % 3;
 
-            System.out.println("Calculate daily wage:" + calcDailyWage);
+                switch (empcheck) {
+                    case part_time:
+                        working_hours=4;
+                        break;
+                    case full_time:
+                        working_hours=8;
+                        break;
+                    default:
+                        working_hours=0;
+                }
+                salary = working_hours * wage_per_hour;
+                totalsalary += salary;
+                System.out.println(" Employee wage is: " +salary);
+            }
+            System.out.println(" Employee totalsalary is: " +totalsalary);
         }
     }
-}
+
+
 
 
 
