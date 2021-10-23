b08725e89c95c68b570870c64cc93bf4ab23e2a6
renuka13599 <renuka13599@gmail.com>
2021-10-23 11:44:03 +0530

    Added:part time wage

--- src/com/bridgelabz/EmpWageComputation.java
+++ src/com/bridgelabz/EmpWageComputation.java
@@ -1,20 +1,31 @@
 package com.bridgelabz;
 
 public class EmpWageComputation {
-        public static final double IS_FULL_TIME=8;
-        public static final double EMP_WAGE_PER_HR=20;
-        public static void main(String[] args)
-        {
-            double Empcheck = (Math.random() * 10) %2 ;
-            if(Empcheck == IS_FULL_TIME)
-                System.out.println("Employee is present");
-            else
-                System.out.println("Employee is absent");
-            double calcDailyWage=0;
-            calcDailyWage=IS_FULL_TIME*EMP_WAGE_PER_HR;
-            System.out.println("Calculate daily wage:" +calcDailyWage);
+    public static final double IS_FULL_TIME = 8;
+    public static final double IS_PART_TIME = 4;
+    public static final double EMP_WAGE_PER_HR = 20;
 
+    public static void main(String[] args) {
+        double Empcheck = (double) Math.floor(Math.random() * 10) % 3;
+        double calcDailyWage = 0;
+        if (Empcheck == 2) {
+            System.out.println("Employee is present");
+            calcDailyWage = IS_FULL_TIME * EMP_WAGE_PER_HR;
+            System.out.printf("Calculate daily wage:" +calcDailyWage);
+
+        } else if (Empcheck == 1) {
+            calcDailyWage = IS_PART_TIME * EMP_WAGE_PER_HR;
+            System.out.println("Part time wage is:");
+            System.out.println("Calculate daily wage:" + calcDailyWage);
+        } else {
+            System.out.println("Employee is absent");
+
+            System.out.println("Calculate daily wage:" + calcDailyWage);
         }
+    }
 }
+
+
+
 
 
